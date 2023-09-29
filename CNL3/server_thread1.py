38b4e5073b376b6d6b1d6abdcedcb3b3e5ec434b
Priyanshu Gupta <112001033@smail.iitpkd.ac.in>
2023-09-29 11:29:57 +0530

    time introduced

--- CNL3/server_thread1.py
+++ CNL3/server_thread1.py
@@ -1,36 +1,60 @@
 import socket
 import threading
+import sys
+import time
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5566 #INPUT
+PORT = int(sys.argv[1])
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 TERMINATION_MSG = "GOODBYE"
 START_MSG = "HELLO"
+TIMEOUT_SECONDS = 60  # Set the initial timeout to 60 seconds
 
 def handle_client(conn, addr):
     # print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     iteration = 0
+    last_msg_time = time.time()  # Track the time of the last received message
+
     while connected:
-        msg = conn.recv(SIZE).decode(FORMAT)
-
-        to_print = msg
-        if(to_print == START_MSG):
-            to_print = "Session created"
-        elif(to_print == TERMINATION_MSG):
-            to_print = "GOODBYE from client"
-        print(f"{addr} [{iteration}] {to_print}")
-
-        if (msg != START_MSG) and (msg != TERMINATION_MSG):
-            msg = "ALIVE"
-        elif (iteration != 0):
+        msg = None
+        try:
+            conn.settimeout(TIMEOUT_SECONDS)  # Set the timeout for this connection
+            msg = conn.recv(SIZE).decode(FORMAT)
+        except socket.timeout:
+            print(f"{addr} Connection timed out (no response). Closing session.")
             connected = False
-            msg = TERMINATION_MSG
-        conn.send(msg.encode(FORMAT))
-        iteration += 1 
+            continue
+        except Exception as e:
+            print(f"{addr} An error occurred: {str(e)}")
+            connected = False
+            continue
+
+        if msg:
+            to_print = msg
+            if to_print == START_MSG:
+                to_print = "Session created"
+            elif to_print == TERMINATION_MSG:
+                to_print = "GOODBYE from client"
+            print(f"{addr} [{iteration}] {to_print}")
+
+            if msg != START_MSG and msg != TERMINATION_MSG:
+                msg = "ALIVE"
+            elif iteration != 0:
+                connected = False
+                msg = TERMINATION_MSG
+            conn.send(msg.encode(FORMAT))
+            iteration += 1
+            last_msg_time = time.time()  # Update the last message time
+
+        # Check if the time since the last message exceeds the timeout
+        if time.time() - last_msg_time > TIMEOUT_SECONDS:
+            print(f"{addr} Connection timed out (no message received). Closing session.")
+            connected = False
+
     print(f"{addr} Session closed")
     conn.close()
 
